f47f2599c7fbc238208d7fa1bc8cb1fd3d764da4
Samuel Gómez <sagomadev@gmail.com>
2023-10-05 10:19:01 +0200

    List products in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,17 @@ import Image from "next/image";
 export default async function Home() {
   const response = await fetch("https://fakestoreapi.com/products");
   const data = await response.json();
-  console.log(data.length);
+  console.log(data);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h2>Products List</h2>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <h3>{item.title}</h3>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
