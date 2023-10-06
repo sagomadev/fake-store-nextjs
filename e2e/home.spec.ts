08ed70d7b6f17c6d36769616324ab791aeb78721
Samuel Gómez <sagomadev@gmail.com>
2023-10-06 10:12:19 +0200

    Add playwright dependency, script & first E2E test

---
+++ e2e/home.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test("should navigate to the about page", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+
+  await expect(page.locator("h1")).toContainText("My Fake Store");
+});
